b904a20fb0cf0cb0334eb8ead79560cd96692ae0
Manel <manelscardoso_94@hotmail.com>
2025-07-10 00:47:58 +0100

    entrypoint.sh and docker-compose updated

--- srcs/mariadb/entrypoint.sh
+++ srcs/mariadb/entrypoint.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -e
 
+mkdir -p /var/lib/mysql /run/mysqld
+chown -R mysql:mysql /var/lib/mysql /run/mysqld
+chmod -R 755 /var/lib/mysql /run/mysqld
+
+if [ ! -d /var/lib/mysql/mysql ]; then
+	mysql_install_db --user=mysql --datadir=/var/lib/mysql
+fi
+
 mysqld --user=mysql --datadir=/var/lib/mysql --socket=/run/mysqld/mysqld.sock --bind-address=0.0.0.0 &
 
 until mysqladmin ping -h localhost -u root -p"${MYSQL_ROOT_PASSWORD}" --silent; do
